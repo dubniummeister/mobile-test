31917a33a5b9567bca2cc3a2823221f1385e04f5
dub <peytonio1127@gmail.com>
2026-08-12 09:34:12 -0500

    testing new pages 8/12/26

---
+++ main_page.gd
@@ -0,0 +1,5 @@
+extends Control
+
+
+func _on_change_page_button_1_pressed() -> void:
+	get_tree().change_scene_to_file("res://control2.tscn")
---
+++ second_page.gd
@@ -0,0 +1,7 @@
+extends Control
+
+
+
+
+func _on_change_page_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://control.tscn")
